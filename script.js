a3ddc5804831fc9c41573de1442fa87286547f0b
25garcial <25garcial@camraiders.com>
2025-02-13 23:23:51 -0600

    update

---
+++ script.js
@@ -0,0 +1,15 @@
+function loadBody(page) {
+  let body = document.querySelector(".body");
+  switch (page) {
+    case "dino-facts":
+      body.innerHTML = `<div class='dino-facts'>
+        <h3>Dino Facts:</h3>
+            <ul style='align-content: center'>
+                <li>The Parasaurolophus is my favorite dinosaur</li>
+                <li>Triceratops horns are made of toenail</li>
+                <li>Pleasiosaur skin was actually found to be a lot like sea turtles' which are their closest relative</li>
+                <li> My <strong style="font-size:28px">favorite </strong>fact!! Not all dinosaurs are birds, but all birds are dinosaurs
+            </ul>
+    </div>`;
+  }
+}
